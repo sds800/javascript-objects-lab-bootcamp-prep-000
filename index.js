90a896a4fd0cf43764ff95c4e2fb569e4489e7d2
SDS 800 <jeremyhyman@gmail.com>
2018-01-14 02:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   
-  
+  object[key] = value
+ 
+  return obj
 }
